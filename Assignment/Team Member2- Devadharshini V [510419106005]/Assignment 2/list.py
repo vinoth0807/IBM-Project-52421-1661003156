bae021d9519e86110244452f300763bb3e2ece8f
Devadharshini017 <113349825+Devadharshini017@users.noreply.github.com>
2022-10-03 16:08:33 +0530

    Add files via upload

---
+++ Assignment/Team Member2- Devadharshini V [510419106005]/Assignment 2/list.py
@@ -0,0 +1,20 @@
+list=['0','2','3','4']
+#insert an integer
+list.insert(1,'1')
+#print the list
+print(list)
+#remove the list
+list.remove('0')
+print(list)
+#append
+list.append('5')
+print(list)
+#sort the list
+list.sort
+print(list)
+#pop the list
+print('the original list is:' +str(list))
+print('the last element using pop() is:' +str(list.pop()))
+#reverse the list
+list.reverse()
+print(reversed list: ',list)
