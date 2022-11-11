30bd887f8926a100dd31b601c43c2276c0f84531
JothikaSelvakumar <113349522+JothikaSelvakumar@users.noreply.github.com>
2022-11-11 11:48:15 +0530

    Add files via upload

---
+++ Assignment/Team Leader-Jothika S [510419106011]/Assignment 2/string.py
@@ -0,0 +1,9 @@
+string1="welcome"
+print(string1)
+#concatenation of string
+string2="Team IBM"
+print(string1+' '+string2)
+#reverse of string
+print(string1[::-1])
+#slicing of string
+print(string1[2:])
